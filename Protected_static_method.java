566e2c6302278cd1f32347b7d4c3fa88f74a3710
Hasini E <hasini1530@gmail.com>
2024-12-11 13:32:13 +0530

    Add files via upload

---
+++ Protected_static_method.java
@@ -0,0 +1,11 @@
+class Protected_static_method
+{
+    protected  static void s() 
+    {
+        System.out.println("HELLO");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+}
