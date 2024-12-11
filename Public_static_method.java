e9548dd499ac2dd22b14e6d36e6c32f3e7987351
Hasini E <hasini1530@gmail.com>
2024-12-11 13:31:58 +0530

    Add files via upload

---
+++ Public_static_method.java
@@ -0,0 +1,11 @@
+class Public_static_method
+{
+    public static void s() 
+    {
+        System.out.println("HELLO");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+}
