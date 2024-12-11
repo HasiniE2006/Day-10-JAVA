6f647b60123ea18b025428d6447d37c482da5591
Hasini E <hasini1530@gmail.com>
2024-12-11 13:31:34 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,13 @@
+class Recursion
+{
+   int sum(int n)
+    {
+        if(n==0) return 0;
+        return n+sum(n-1);
+    }
+    public static void main(String[] args) {
+        Recursion r=new Recursion();
+        int res=r.sum(10);
+        System.out.println(res);
+    }
+}
