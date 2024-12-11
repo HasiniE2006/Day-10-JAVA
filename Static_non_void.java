a04683d665a68d4a38a19993a7127bb8196d7c79
Hasini E <hasini1530@gmail.com>
2024-12-11 13:31:19 +0530

    Add files via upload

---
+++ Static_non_void.java
@@ -0,0 +1,11 @@
+class Static_non_void{
+    public static String s() 
+    {
+      return "HELLO";
+    }
+   public static void main(String[] args) {
+       String res = s();
+       System.out.println(res);
+    }
+ }
+ 
