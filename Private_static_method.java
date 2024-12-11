96a3b607a3d80b155bc95b5ad9d7113c92caadf6
Hasini E <hasini1530@gmail.com>
2024-12-11 13:32:49 +0530

    Add files via upload

---
+++ Private_static_method.java
@@ -0,0 +1,11 @@
+class Private_static_method 
+{
+    private  static void s() 
+    {
+        System.out.println("HELLO");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+}
